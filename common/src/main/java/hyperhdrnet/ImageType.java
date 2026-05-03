38191df86715116d34e40b708bf77b054724cc10
John Neerdael <jneerdael@netskope.com>
2026-05-03 13:35:39 +0200

    build: bump to 1.1.0-dev; sync hyperhdr_request.fbs + regenerate hyperhdrnet for P010

--- common/src/main/java/hyperhdrnet/ImageType.java
+++ common/src/main/java/hyperhdrnet/ImageType.java
@@ -8,8 +8,9 @@ public final class ImageType {
   public static final byte NONE = 0;
   public static final byte RawImage = 1;
   public static final byte NV12Image = 2;
+  public static final byte P010Image = 3;
 
-  public static final String[] names = { "NONE", "RawImage", "NV12Image", };
+  public static final String[] names = { "NONE", "RawImage", "NV12Image", "P010Image", };
 
   public static String name(int e) { return names[e]; }
 }
